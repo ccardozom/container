5113b7b9c5506a040eb9576be9613bf4960c2f38
ccardozom <carloscardozom@gmail.com>
2022-08-10 17:31:49 +0200

    add comments in stack.hpp

--- Stack/stack.hpp
+++ Stack/stack.hpp
@@ -7,11 +7,14 @@ namespace ft {
     template <class T, class Container = std::vector<T> > 
     class stack{
         public:
-            //como sabemos que Container es equivalente a decir vector<T> donde T es el tipo de dato
+            //sabemos que Container es equivalente a decir vector<T> donde T es el tipo de dato
             //la siguiente linea genera un alias a Container que a partir de ahora será container_type
             typedef Container   container_type;
-            //aqui creamos un alias para Container::value_type que es el tipo de dato o tambien T en este caso
+            //aqui creamos un alias para Container::value_type que es el tipo de dato
             //a partir de ahora T sera value_type
+            //Si necesitamos hacer uso de un tipo proporcionado por la clase/estructura utilizada para especializar el template tendremos 
+            //que redeclarar el tipo en nuestro template. En este caso tendremos que usar obligatoriamente typename
+            // aqui esta un comentario que puede ayudar a entender mejor esta definicion https://es.stackoverflow.com/questions/540/son-iguales-class-y-typename-en-el-contexto-de-template
             typedef typename Container::value_type  value_type;
             //es lo mismo que size_t y sirve para representar el tamaño del objeto en forma de un entero positivo 
             //con valor de 0 a 4.000.000.000 aproximadamente
@@ -26,7 +29,9 @@ namespace ft {
         public:
             //Constructores
             // este constructor es para instanciar un objeto vacio
-            // Ejemplo: stack(int) pila; --> donde vector<T> &ctnr = vector<T>()
+            //la palabra reservada explicit cumple la funcion de avisar al compilar que la llamada a este constructor tiene que ser explicita y no implicita
+            //en este enlace puedes encontrar un ejemplo claro del porque y para que se utiliza la palabra reservada http://ambar.com.ve/saphire/?p=505
+            // Ejemplo: stack(int) pila; -->esto llama al constructor donde vemos que al cambiar los alias quedaria de esta forma vector<T> &ctnr = vector<T>()
             explicit stack (const container_type& ctnr = container_type()){
                 c = ctnr;  
             }
