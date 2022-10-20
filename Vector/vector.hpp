836d772b1c1bba98fd6daffffcc4dbeef088e61f
ccardozom <carloscardozom@gmail.com>
2022-10-20 13:22:31 +0200

    finish stack container

--- Vector/vector.hpp
+++ Vector/vector.hpp
@@ -8,13 +8,12 @@ namespace ft {
    class vector{
        typedef T value_type;
        typedef Alloc allocator_type;
-       typedef typename allocator_type::reference reference;
-       typedef typename allocator_type::const_reference const_reference;
-       typedef typename allocator_type::pointer pointer;
-       typedef typename allocator_type::const_pointer const_pointer;
+       typedef allocator_type::reference reference;
+       typedef allocator_type::const_reference const_reference;
+       typedef allocator_type::pointer pointer;
+       typedef allocator_type::const_pointer const_pointer;
        typedef value_type iterator;
        
-
    };
 }
 
--- main.cpp
+++ main.cpp
@@ -21,8 +21,8 @@ int main(){
   ft::stack<int> pila2;
   ft::stack<int> pila3;
 
-// ***********  PRUEBAS PARA EL CONTENEDOR STACK  ***********
-
+  // ***********  PRUEBAS PARA EL CONTENEDOR STACK  ***********
+  std::cout << "*******  PRUEBAS PARA EL CONTENEDOR STACK  *******" << std::endl;
   std::cout << "\n========= push =============" << std::endl; //prueba del metodo PUSH
   std::cout << "Añadimos un elemento a pila1 con el metodo push de esta forma --> pila1.push(5);" << std::endl;
   pila1.push(5);
@@ -74,15 +74,40 @@ int main(){
   std::cout << "\n========= size =============" << std::endl; //prueba del metodo SIZE
   std::cout << "El Objeto pila2 tiene " << pila2.size() << " elementos" << std::endl;
   std::cout << "El Objeto pila1 tiene " << pila1.size() << " elementos" << std::endl;
+  ft::stack<int> myints;
+  std::cout << "0. size: " << myints.size() << '\n';
+  for (int i=0; i<5; i++) 
+    myints.push(i);
+  std::cout << "1. size: " << myints.size() << '\n';
+  myints.pop();
+  std::cout << "2. size: " << myints.size() << '\n';
 
   std::cout << "\n========= pop =============" << std::endl; //prueba del metodo POP
   std::cout << "Quitamos el ultimo elemento a pila1 con el metodo pop de esta forma --> pila1.pop();" << std::endl;
   pila1.pop();
   std::cout << "El Objeto pila1 tiene " << pila1.size() << " elementos" << std::endl;
+  ft::stack<int> mystack_pop;
+  for (int i=0; i<5; ++i) 
+    mystack_pop.push(i);
+  std::cout << "El tamaño de mystack_pop es: " << mystack_pop.size() << std::endl;
+  std::cout << "mystack_pop contiene: ";
+  while (!mystack_pop.empty())
+  {
+    std::cout << ' ' << mystack_pop.top();
+    mystack_pop.pop();
+  }
+  std::cout << '\n';
+  std::cout << "Despues de vaciar con pop el tamaño de mystack_pop es: " << mystack_pop.size() << std::endl;
+  std::cout << '\n';
 
-// ----------  SOBRECARGA DE OPERADORES  ----------
+
+  // ----------  SOBRECARGA DE OPERADORES  ----------
+  std::cout << "\n===== SOBRECARGA DE OPERADORES =====" << std::endl;
   bool test = (pila2 == pila1); //prueba del operador ==
-  std::cout << test << " son iguales" <<std::endl;
+  if (test == 1)
+    std::cout << "Los objetos pila2 y pila1 son iguales" << std::endl;
+  else
+    std::cout << "Los objetos pila2 y pila1 son diferentes" << std::endl;
   pila1.push(10);
   test = (pila2 < pila1); //prueba del operador <
   if (test == 1)
@@ -94,6 +119,26 @@ int main(){
     std::cout << "pila2 es mayor que pila1" <<std::endl;
   else
     std::cout << "pila2 es menor que pila1" <<std::endl;
+  test = (pila2 <= pila1); //prueba del operador <=
+  if (test == 1)
+    std::cout << "pila2 es menor o igual a pila1" <<std::endl;
+  else
+    std::cout << "pila2 es mayor a pila1" <<std::endl;
+  test = (pila2 >= pila1); //prueba del operador >=
+  if (test == 1)
+    std::cout << "pila2 es mayor o igual a pila1" <<std::endl;
+  else
+    std::cout << "pila2 es menor a pila1" <<std::endl;
+  test = (pila2 != pila1); //prueba del operador !=
+  if (test == 1)
+    std::cout << "pila2 es distinto a pila1" <<std::endl;
+  else
+    std::cout << "pila2 es igual a pila1" <<std::endl;
+
+  std::cout << "*******  PRUEBAS PARA EL CONTENEDOR VECTOR  *******" << std::endl;
+
+  
+
 
   return 0;
 }
